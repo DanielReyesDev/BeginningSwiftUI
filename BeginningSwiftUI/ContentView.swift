3dcf680d40b6fa7bb68bcf581b47bdab6702cca1
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-06-05 08:44:18 -0500

    Beginning with SwiftUI basics

---
+++ BeginningSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BeginningSwiftUI
+//
+//  Created by Daniel Reyes Sánchez on 05/06/19.
+//  Copyright © 2019 Daniel Reyes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
